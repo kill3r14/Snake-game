eca749d8635c878b0099906f3fa2776c3715d430
kill3r14 <kill3r14@users.noreply.github.com>
2020-04-28 09:15:46 +0530

    added die function

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
-var s;
 var scl = 20;
+var s;
 var food;
+
 function setup(){
   createCanvas(600,600);
   s = new Snake();
@@ -8,31 +9,31 @@ function setup(){
   pickLocation();
 }
 function pickLocation(){
-  var cols = floor(width/scl);
-  var rows = floor(height/scl);
-
+  cols = floor(width/scl);
+  rows = floor(height/scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
-  food.mult(scl)
-}
-function draw(){
-  background(51);
-  s.update();
-  s.show();
-
-  if(s.eat(food)){
-    pickLocation()
-  }
-  fill(255,0,100);
-  rect(food.x, food.y, scl , scl)
+  food.mult(scl);
 }
 function keyPressed(){
   if(keyCode === UP_ARROW){
     s.dir(0,-1);
-  } else if (keyCode === DOWN_ARROW){
-    s.dir(0,1);
-  }else if (keyCode === LEFT_ARROW){
-    s.dir(-1,0);
-  }else if (keyCode === RIGHT_ARROW){
-    s.dir(1,0);
+  } else if ( keyCode === DOWN_ARROW){
+    s.dir(0,1)
+  } else if ( keyCode === LEFT_ARROW){
+    s.dir(-1,0)
+  } else if ( keyCode === RIGHT_ARROW){
+    s.dir(1,0)
   }
+}
+function draw (){
+  background(51);
+  if(s.eat(food)){
+    pickLocation();
+  }
+  s.die();
+  s.update();
+  s.show();
+
+  fill(255,0,100);
+  rect(food.x, food.y, scl,scl)
 }
